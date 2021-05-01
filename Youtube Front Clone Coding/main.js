ceb3eacd4082dd3beb625eecd57c8a242b2c36e2
llshl <52540882+llshl@users.noreply.github.com>
2021-05-02 00:27:43 +0900

    Delete Youtube Front Clone Coding directory

--- Youtube Front Clone Coding/main.js
+++
@@ -1,7 +0,0 @@
-const moreBnt = document.querySelector('.info .metadata .moreBnt');
-const title = document.querySelector('.info .metadata .title');
-
-moreBnt.addEventListener('click',() => {
-  moreBnt.classList.toggle('clicked');
-  title.classList.toggle('clamp');
-});
